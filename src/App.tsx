dac017463a5033d05b269f5f2e64636e4a8837b2
Yves Gurcan <gurcan.yves+github@gmail.com>
2023-07-01 17:05:57 -0700

    Add tablet display

--- src/App.tsx
+++ src/App.tsx
@@ -79,6 +79,10 @@ const Days = styled.div`
     grid-gap: 5rem;
     width: 100vw;
 
+    @media (max-width: 1000px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
     @media (max-width: 700px) {
         grid-template-columns: 1fr;
     }
